b87b0fa3127b4cc9454c24f6e0e871e362ddb432
cahyadie <cahyadiilham12@gmail.com>
2023-06-19 10:54:51 +0700

    menambahkan nama

---
+++ iofilewithpath/iofilewithpath.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile,ios::out);
+
+	cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+
+}
