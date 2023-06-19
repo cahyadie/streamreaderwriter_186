1b932eb009a0a6f28d574a3bc8da634cc862e76e
cahyadie <cahyadiilham12@gmail.com>
2023-06-19 10:36:40 +0700

    mengatur file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	string basris;
+	string baris;
 
 	//membuka file dalam mode menulis.
 	ofstream outfile;
@@ -15,6 +15,24 @@ int main() {
 	//unlimited loop untuk menulis
 	while (true) {
 		cout << "_";
-		
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter 
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari baris ke file
+	}
+	//selasi dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//membuka file dalam mode baca 
+	ifstream infile;
+	//menunjuk kesebuah file
+	infile.open("contohfile.txt");
+
+		cout << endl << ">= Membuka dan Membaca file" << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+
 	}
 } 
