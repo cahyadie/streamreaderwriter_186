6320fd0928d4bf57e6ca86e230737ecb1e1eebfc
cahyadie <cahyadiilham12@gmail.com>
2023-06-19 11:16:27 +0700

    make catch

--- ManualExeption/ManualExeption.cpp
+++ ManualExeption/ManualExeption.cpp
@@ -7,4 +7,12 @@ int main() {
 		//throw 0,5; //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "defaault pengecualian dieksekusi" << endl;
+	}
 }
