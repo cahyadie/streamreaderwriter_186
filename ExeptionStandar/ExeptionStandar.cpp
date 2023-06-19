6805aaf90e35b1b73ac8e2ba9287d6db7c9205f0
cahyadie <cahyadiilham12@gmail.com>
2023-06-19 11:26:32 +0700

    membuat trynya

--- ExeptionStandar/ExeptionStandar.cpp
+++ ExeptionStandar/ExeptionStandar.cpp
@@ -5,3 +5,14 @@
 //untuk obyek array yang akan kita gunakan
 using namespace std;
 
+
+int main() {
+	cout << "Selamat Belajar di prodi TI UMY" << endl;
+	try {
+		array<int, 3> data = { 1,2,3 };
+		//pesan array integer 3 element
+		//cout<<data.at(5)<<endl;
+		//memanggil array element ke 5
+	}
+	
+}
