6757c64df7adaed27b5422c1d26d4cf58bcd59d5
cahyadie <cahyadiilham12@gmail.com>
2023-06-19 10:47:53 +0700

    done and some fix

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -31,8 +32,18 @@ int main() {
 
 		cout << endl << ">= Membuka dan Membaca file" << endl;
 	//jika file ada maka
-	if (infile.is_open())
-	{
-
-	}
+		if (infile.is_open())
+		{
+			//melakukan perulangan setiap baris 
+			while (getline(infile, baris))
+			{
+				//dan tampilkan di sini
+				cout << baris << '\n';
+			}
+			//tutup file tersebut setelah selsai
+			infile.close();
+		}
+		//jika tidak di temukan file maka akan menampilkan ini
+		else cout << "Unable to open file";
+		return 0;
 } 
